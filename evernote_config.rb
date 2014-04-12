2c8d2e5f3c2073297bf1972debcb60c764401f05
johan pretorius <johan.pretorius@gmail.com>
2014-04-12 09:33:45 +0200

    updated evernote key info

--- evernote_config.rb
+++ evernote_config.rb
@@ -3,13 +3,13 @@ require 'oauth'
 require 'oauth/consumer'
 
 # Load Thrift & Evernote Ruby libraries
-require "evernote_oauth"
+require 'evernote_oauth'
 
 # Client credentials
 # Fill these in with the consumer key and consumer secret that you obtained
 # from Evernote. If you do not have an Evernote API key, you may request one
 # from http://dev.evernote.com/documentation/cloud/
-OAUTH_CONSUMER_KEY = "your key"
-OAUTH_CONSUMER_SECRET = "your secret"
+OAUTH_CONSUMER_KEY = 'xxxxxxxxxxxxxxx'
+OAUTH_CONSUMER_SECRET = 'xxxxxxxxxxxxxxx'
 
 SANDBOX = true
